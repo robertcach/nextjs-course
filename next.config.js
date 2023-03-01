e7a1feac7fd901df79ff34834af9f73cf2599cd6
Robert Cabrera <cabrera.rch@gmail.com>
2023-03-01 17:51:09 +0100

    Add API domain to next.config file to can show images from API

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     appDir: true,
   },
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{ hostname: 'rickandmortyapi.com' }],
+  },
 };
 
 module.exports = nextConfig;
