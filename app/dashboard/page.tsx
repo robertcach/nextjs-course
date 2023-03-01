a4009e36eecdf331ff55916ee9571631bad876af
Robert Cabrera <cabrera.rch@gmail.com>
2023-03-01 17:50:09 +0100

    Creat a basic async/await fetch

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
-export default function Dashboard() {
-  return <p>Dashboard</p>;
+import Image from 'next/image';
+
+async function getData() {
+  const response = await fetch('https://rickandmortyapi.com/api/character/2');
+  return response.json();
+}
+export default async function Dashboard() {
+  const data = await getData();
+  return (
+    <div>
+      <Image width={100} height={100} src={data.image} alt="Mort" />
+    </div>
+  );
 }
